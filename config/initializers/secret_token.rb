f7ad9f1867f207fe0a39a1217625e44314058829
Jacob Vass <jacobvass@gmail.com>
2012-08-14 01:08:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkos::Application.config.secret_token = 'b7375ebb206187a9e6b10c95ce1d85cd30280433bc5f65a193bea965587c8a83757e8a9cdbf8ddf2d5f1f72519d26489780735afea43ff119dc71470ba1c9bcb'
